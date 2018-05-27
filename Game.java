657e67fc68ede6c5d6c8ba9c471361b92ef19b96
sevusdrags <39673343+sevusdrags@users.noreply.github.com>
2018-05-27 22:45:34 +0800

    not much

---
+++ Game.java
@@ -0,0 +1,12 @@
+package game;
+
+import java.awt.Color;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {    
+     new GameEngine();
+     
+    }
+    
+}
